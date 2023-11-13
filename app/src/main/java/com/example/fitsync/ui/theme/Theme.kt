d3091b35c618a1ecd5b79dc1c03c5db3a0da253c
Arthur Martins <arthurcarbonari99@gmail.com>
2023-11-13 12:56:17 +0000

    Change app Theme

--- app/src/main/java/com/example/fitsync/ui/theme/Theme.kt
+++ app/src/main/java/com/example/fitsync/ui/theme/Theme.kt
@@ -10,36 +10,46 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
+    primary = Color(0xFFF44336),         // Blue Grey-ish
+    onPrimary = Color(0xFF000000),       // Black for text/icons on primary color
+    primaryContainer = Color(0xFF121212),// Very dark grey, almost black
+    secondary = Color(0xFF80CBC4),       // Soft Green/Teal
+    onSecondary = Color(0xFF000000),     // Black for text/icons on secondary color
+    tertiary = Color(0xFF553025),        // Soft Orange
+    onTertiary = Color(0xFF000000),      // Black for text/icons on tertiary color
+    background = Color(0xFF212121),      // Deep Grey
+    onBackground = Color(0xFFFFFFFF),    // White for text/icons on background
+    surface = Color(0xFF212121),         // Deep Grey (same as background)
+    onSurface = Color(0xFFFFFFFF),       // White for text/icons on surface
+    error = Color(0xFFCF6679),           // Soft Red
+    onError = Color(0xFF000000)         // Black for text/icon on error color
+    // You can define other colors like surfaceVariant, outline, etc.
 )
 
 private val LightColorScheme = lightColorScheme(
         primary = Purple40,
         secondary = PurpleGrey40,
-        tertiary = Pink40
+        tertiary = Pink40,
+        background = Color(0xFFFFFBFE),
+        surface = Color(0xFFFFFBFE),
+        onPrimary = Color.White,
+        onSecondary = Color.White,
+        onTertiary = Color.White,
+        onBackground = Color(0xFF1C1B1F),
+        onSurface = Color(0xFF1C1B1F),
 
-        /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun FitSyncTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
+        darkTheme: Boolean = true,
         // Dynamic color is available on Android 12+
         dynamicColor: Boolean = true,
         content: @Composable () -> Unit
@@ -57,8 +67,10 @@ fun FitSyncTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Color.Transparent.toArgb()
+            window.navigationBarColor = Color(0xFF272429).toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.setDecorFitsSystemWindows(window, false)
         }
     }
 
