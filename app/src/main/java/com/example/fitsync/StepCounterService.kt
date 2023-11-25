a44020830407bbd8cf4217f7bfdc3df518afe185
Arthur Martins <arthurcarbonari99@gmail.com>
2023-11-25 22:51:07 +0000

    Implement StepCounterService

--- app/src/main/java/com/example/fitsync/StepCounterService.kt
+++ app/src/main/java/com/example/fitsync/StepCounterService.kt
@@ -3,10 +3,97 @@ package com.example.fitsync
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.app.PendingIntent
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
+import androidx.annotation.RequiresApi
 
-class StepCounterService : Service() {
+class StepCounterService : Service(), SensorEventListener {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    private lateinit var sensorManager: SensorManager
+    private var stepSensor: Sensor? = null
+    private var totalSteps = 0
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+
+        if (stepSensor != null) {
+            Log.d("StepCounterService", "Step detector sensor is available")
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_FASTEST)
+        } else {
+            Log.d("StepCounterService", "Step detector sensor is not available")
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.d("StepCounterService", "Service is running")
+        createNotificationChannel()
+        val notification = createNotification()
+        startForeground(NOTIFICATION_ID, notification)
+
+        return START_STICKY
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        Log.d("StepCounterService", "Sensor changed here")
+
+        if (event.sensor.type == Sensor.TYPE_STEP_DETECTOR) {
+            // Increment the step count for each step detected
+            totalSteps++
+            Log.d("StepCounterService", "Total Steps: $totalSteps")
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID,
+                "Step Counter Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotification(): Notification {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+
+        return Notification.Builder(this, CHANNEL_ID)
+            .setContentTitle("Step Counter Running")
+            .setContentText("Counting your steps...")
+            .setSmallIcon(R.drawable.ic_launcher_foreground) // Replace with your own icon
+            .setContentIntent(pendingIntent)
+            .build()
+    }
+
+    override fun onDestroy() {
+        Log.d("StepCounterService", "Service is being destroyed")
+        super.onDestroy()
+        sensorManager.unregisterListener(this)
+    }
+
+    companion object {
+        const val CHANNEL_ID = "StepCounterServiceChannel"
+        const val NOTIFICATION_ID = 1
     }
 }
