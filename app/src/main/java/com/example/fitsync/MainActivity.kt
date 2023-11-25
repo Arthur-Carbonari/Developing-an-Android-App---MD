201bc1da513d650be22691eaefbbd5b385291546
Arthur Martins <arthurcarbonari99@gmail.com>
2023-11-25 22:51:28 +0000

    Initiates StepCounterService on create

--- app/src/main/java/com/example/fitsync/MainActivity.kt
+++ app/src/main/java/com/example/fitsync/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fitsync
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -34,7 +35,21 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        startStepCounterService()
     }
+
+    private fun startStepCounterService() {
+        val serviceIntent = Intent(this, StepCounterService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent)
+        } else {
+            startService(serviceIntent)
+        }
+    }
+
+
+
 }
 
 // Preview function
