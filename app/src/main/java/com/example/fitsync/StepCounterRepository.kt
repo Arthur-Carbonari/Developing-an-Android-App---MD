f0b28c902bb1dcc5494224f40a3abb4d7559475a
Arthur Martins <arthurcarbonari99@gmail.com>
2023-11-27 16:16:27 +0000

    Refactor init method to store current day on shared preferences

--- app/src/main/java/com/example/fitsync/StepCounterRepository.kt
+++ app/src/main/java/com/example/fitsync/StepCounterRepository.kt
@@ -4,6 +4,9 @@ import android.content.SharedPreferences
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 object StepCounterRepository {
     private val _stepsFlow = MutableStateFlow(0)
@@ -13,7 +16,19 @@ object StepCounterRepository {
 
     fun init(context: Context) {
         sharedPreferences = context.getSharedPreferences("StepCounterPrefs", Context.MODE_PRIVATE)
-        _stepsFlow.value = sharedPreferences.getInt("steps", 0)
+
+        val stepsCurrentDay = sharedPreferences.getString("currentDay", "")
+        val actualCurrentDay = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+
+        if (stepsCurrentDay != actualCurrentDay) {
+            _stepsFlow.value = 0
+            sharedPreferences.edit()
+                .putInt("steps", 0)
+                .putString("currentDay", actualCurrentDay)
+                .apply()
+        } else {
+            _stepsFlow.value = sharedPreferences.getInt("steps", 0)
+        }
     }
 
     fun updateSteps(steps: Int) {
