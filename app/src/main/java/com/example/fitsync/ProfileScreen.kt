a8465a9b2ba2d9fb67c7d2e3b7a26c0393efaeb9
Arthur Martins <arthurcarbonari99@gmail.com>
2023-11-27 21:18:37 +0000

    Refactor ProfileScreen to display information from actual from FireAuth User

--- app/src/main/java/com/example/fitsync/ProfileScreen.kt
+++ app/src/main/java/com/example/fitsync/ProfileScreen.kt
@@ -1,27 +1,47 @@
 package com.example.fitsync
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.material3.Icon
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.foundation.layout.*
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun ProfileScreen() {
+    val user = FirebaseAuth.getInstance().currentUser
+    val userName = user?.displayName ?: "Guest"
+    val profilePhotoUrl = user?.photoUrl
     Column(
         modifier = Modifier.fillMaxWidth().fillMaxHeight().padding(16.dp),
         verticalArrangement = Arrangement.SpaceBetween,
@@ -35,15 +55,23 @@ fun ProfileScreen() {
             verticalArrangement = Arrangement.Center,
         ) {
             Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
+                painter = // Fallback image on error
+                rememberAsyncImagePainter(
+                    ImageRequest.Builder(LocalContext.current).data(data = profilePhotoUrl)
+                        .apply(block = fun ImageRequest.Builder.() {
+                            crossfade(true)
+                            error(R.drawable.ic_launcher_background) // Fallback image on error
+                        }).build()
+                ),
                 contentDescription = "Profile Picture",
                 modifier = Modifier
                     .size(150.dp)
-                    .clip(CircleShape)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
             )
             Spacer(modifier = Modifier.height(16.dp))
 
-            Text("Your Name", style = MaterialTheme.typography.titleLarge)
+            Text(userName, style = MaterialTheme.typography.titleLarge)
             Text(
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
                 style = MaterialTheme.typography.bodySmall,
@@ -61,6 +89,28 @@ fun ProfileScreen() {
             ProfileOptionItem(title = "User", icon = Icons.Filled.Person)
             ProfileOptionItem(title = "Health", icon = Icons.Filled.Favorite)
             ProfileOptionItem(title = "Account Settings", icon = Icons.Filled.Settings)
+            Card(
+                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp).clickable {
+                    FirebaseAuth.getInstance().signOut()
+                },
+                shape = MaterialTheme.shapes.large,
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant)
+            ) {
+                Row(
+                    modifier = Modifier.padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Filled.ExitToApp, contentDescription = null, modifier = Modifier.size(24.dp))
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text("Logout", style = MaterialTheme.typography.titleMedium)
+                    Spacer(modifier = Modifier.weight(1f))
+//                    Icon(
+//                        imageVector = Icons.Filled.ArrowForward,
+//                        contentDescription = "Go to $title"
+//                    )
+                }
+            }
+
         }
 
 
