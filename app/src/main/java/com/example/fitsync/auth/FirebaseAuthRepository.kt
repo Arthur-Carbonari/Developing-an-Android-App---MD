cd5e074d706042542577173bf235f9271cf2efac
Arthur Martins <arthurcarbonari99@gmail.com>
2023-12-14 12:16:32 +0000

    Add getAuthStateFlow method to Repository

--- app/src/main/java/com/example/fitsync/auth/FirebaseAuthRepository.kt
+++ app/src/main/java/com/example/fitsync/auth/FirebaseAuthRepository.kt
@@ -3,11 +3,24 @@ package com.example.fitsync.auth
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import javax.inject.Inject
 
 class FirebaseAuthRepository @Inject constructor() {
     private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    fun getAuthStateFlow(): Flow<FirebaseUser?> = callbackFlow {
+        val authStateListener = FirebaseAuth.AuthStateListener { auth ->
+            trySend(auth.currentUser).isSuccess
+        }
+
+        firebaseAuth.addAuthStateListener(authStateListener)
+        awaitClose { firebaseAuth.removeAuthStateListener(authStateListener) }
+    }
 
     fun firebaseAuthWithGoogle(idToken: String): Task<AuthResult> {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
