05c9f9ed42ec68f6b8ef7af8431eea0dd3eeebb6
Arthur Martins <arthurcarbonari99@gmail.com>
2023-12-14 12:17:54 +0000

    Add check if user is logged to activity

--- app/src/main/java/com/example/fitsync/SplashActivity.kt
+++ app/src/main/java/com/example/fitsync/SplashActivity.kt
@@ -4,18 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.fitsync.auth.AuthActivity
+import com.example.fitsync.auth.FirebaseAuthRepository
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SplashActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var firebaseAuthRepository: FirebaseAuthRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Handle the splash screen transition.
-        installSplashScreen().apply {
-            setKeepOnScreenCondition { true }
-        }
+        installSplashScreen().apply { setKeepOnScreenCondition { true } }
 
         super.onCreate(savedInstanceState)
 
-        // Navigate to MainActivity after initialization is complete.
+        // Check if the user is signed in and navigate accordingly
+        if (firebaseAuthRepository.isUserSignedIn()) {
+            navigateToMainActivity()
+        } else {
+            navigateToAuthActivity()
+        }
+    }
+
+    private fun navigateToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun navigateToAuthActivity() {
         startActivity(Intent(this, AuthActivity::class.java))
         finish()
     }
