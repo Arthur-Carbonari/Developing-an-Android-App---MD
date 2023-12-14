3c42c437ca4839a8e1ef5b330c89ccdf6a8c088b
Arthur Martins <arthurcarbonari99@gmail.com>
2023-12-14 19:26:45 +0000

    Refactor StepCounterRepository

--- app/src/main/java/com/example/fitsync/steps/StepCounterRepository.kt
+++ app/src/main/java/com/example/fitsync/steps/StepCounterRepository.kt
@@ -1,49 +1,65 @@
 package com.example.fitsync.steps
-import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.example.fitsync.steps.models.DaySteps
+import com.example.fitsync.utils.getWeekId
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import java.time.LocalDate
 import javax.inject.Inject
 import javax.inject.Singleton
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Singleton
 class StepCounterRepository @Inject constructor(
-    private val sharedPreferences: SharedPreferences
+    private val sharedPreferences: SharedPreferences,
+    private val firestoreStepRepository: FirestoreStepRepository // Firestore repository
 ) {
     private val _stepsFlow = MutableStateFlow(0)
     val stepsFlow: StateFlow<Int> = _stepsFlow.asStateFlow()
 
-    private var currentDay = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+    private var currentDay = LocalDate.now()
 
     init {
-        val stepsCurrentDay = sharedPreferences.getString("currentDay", "")
-        if (stepsCurrentDay != currentDay) startCurrentDay()
-        else _stepsFlow.value = sharedPreferences.getInt("steps", 0)
+        val storedDay = sharedPreferences.getString("currentDay", null)
+        val storedDayDate = LocalDate.parse(storedDay)
+        if (storedDayDate != currentDay) {
+            storePreviousDaySteps(storedDayDate)
+            startCurrentDay()
+        } else {
+            _stepsFlow.value = sharedPreferences.getInt("steps", 0)
+        }
     }
 
     fun incrementSteps() {
-        if (_stepsFlow.value % 50 == 0) {
-            val actualCurrentDay = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
-            if (actualCurrentDay != currentDay) {
-                currentDay = actualCurrentDay
-                startCurrentDay()
-            }
-            _stepsFlow.value++
-            sharedPreferences.edit().putInt("steps", _stepsFlow.value).apply()
-            return
-        }
         _stepsFlow.value++
+        val newDay = LocalDate.now()
+        if (newDay != currentDay) {
+            storePreviousDaySteps(currentDay)
+            currentDay = newDay
+            startCurrentDay()
+        } else if (_stepsFlow.value % 50 == 0) {
+            updateSharedPreferences()
+        }
+    }
+
+    private fun storePreviousDaySteps(date: LocalDate) {
+        val weekId = date.getWeekId()
+        val daySteps = DaySteps(date.toString(), _stepsFlow.value)
+        firestoreStepRepository.saveStepsForDay(weekId, daySteps)
+    }
+
+    private fun updateSharedPreferences() {
+        sharedPreferences.edit()
+            .putInt("steps", _stepsFlow.value)
+            .putString("currentDay", currentDay.toString())
+            .apply()
     }
 
     private fun startCurrentDay() {
         _stepsFlow.value = 0
-        sharedPreferences.edit()
-            .putInt("steps", 0)
-            .putString("currentDay", currentDay)
-            .apply()
+        updateSharedPreferences()
     }
 }
