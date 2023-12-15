3e5eff634d8529bcb1def907da6ef3b738baed48
Arthur Martins <arthurcarbonari99@gmail.com>
2023-12-15 03:57:15 +0000

    Refactor LoadingOverlay

--- app/src/main/java/com/example/fitsync/utils/LoadingOverlay.kt
+++ app/src/main/java/com/example/fitsync/utils/LoadingOverlay.kt
@@ -1,18 +1,30 @@
 package com.example.fitsync.utils
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 
 @Composable
 fun LoadingOverlay() {
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier
+            .fillMaxSize().zIndex(10f)
+            .background(Color.Black.copy(alpha = 0.6f)),
+        contentAlignment = Alignment.Center,
     ) {
-        CircularProgressIndicator() // You can customize this as per your design
+        CircularProgressIndicator(
+            strokeWidth = 5.dp,
+            modifier = Modifier.height(30.dp),
+            color = MaterialTheme.colorScheme.primary
+        )
     }
 }
